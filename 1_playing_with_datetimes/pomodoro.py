e05d39e88897c916c87e2870c12e897bea5451d6
Corey Hermanson <corey.hermanson@gmail.com>
2018-04-04 09:46:01 -0700

    Day 3: Pomodoro Timer working. Pulled most logic out of the WHILE loop. WHILE is only checking if current_time > alarm_time

--- 1_playing_with_datetimes/pomodoro.py
+++ 1_playing_with_datetimes/pomodoro.py
@@ -1,12 +1,7 @@
+import time
 from datetime import datetime
 from datetime import timedelta
 import itertools
-
-
-def header():
-    print('==========================================================')
-    print('==== Pomodoro Timer using only date and datetime ====')
-    print('==========================================================')
 
 
 def main():
@@ -17,28 +12,39 @@ def main():
     timer()
 
 
+def header():
+    print('==========================================================')
+    print('====== Pomodoro Timer using only date and datetime ======')
+    print('==========================================================')
+    print('')
+    print('Press CTRL + C to cancel the timer')
+    print('')
+
+
 def timer():
     sequence = [25, 5, 25, 5, 25, 5, 25, 20, ]
 
     try:
-        while True:
-            work_time = True
-            for duration in itertools.cycle(sequence):
+        work_time = True
+
+        for duration in itertools.cycle(sequence):
+            alarm_time = datetime.now() + timedelta(minutes=duration)
+
+            if work_time:
+                print(f"Time to work! The current time is {datetime.now().strftime('%H:%M')}. "
+                      f"The current work period is {duration} minutes and the next alarm will go"
+                      f" off at {alarm_time.strftime('%H:%M')}")
+            else:
+                next_work = datetime.now() + timedelta(minutes=duration)
+                print(f"Break time! Take a break for {duration} minutes. "
+                      f"You will resume working at {next_work.strftime('%H:%M:%S')}")
+
+            while True:
+                time.sleep(1)
                 current_time = datetime.now()
-                alarm_time = current_time + timedelta(minutes=duration)
-                print(f"The current time is {current_time.strftime('%H:%M')}. The next alarm will go off at "
-                      f"{alarm_time.strftime('%H:%M')}")
-
                 if current_time > alarm_time:
-                    if work_time:
-                        next_break = current_time + timedelta(minutes=25)
-                        print(f"Time to get to work for 25 minutes. Your next break will be at "
-                              f"{next_break}")
-                    else:
-                        next_work = current_time + timedelta(minutes=duration)
-                        print(f"Break time for {duration} minutes. You will resume working at {next_work}")
-
                     work_time = not work_time
+                    break
 
     except KeyboardInterrupt:
         print("You have aborted the Pomodoro Timer")
